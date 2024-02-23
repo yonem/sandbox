3d61182da095ae57037d2c187d3c4deda94eeb59
yonem <yonem321@gmail.com>
2024-02-24 07:16:14 +0900

    feat: マルチスレッドのサンプルコードを追加する #21

---
+++ src/main/java/jp/ne/yonem/sandbox/MultithreadSample.java
@@ -0,0 +1,40 @@
+package jp.ne.yonem.sandbox;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class MultithreadSample {
+
+    public static void main(String[] args) {
+        var availableProcessors = Runtime.getRuntime().availableProcessors();
+
+        try (var threadPool = Executors.newFixedThreadPool(availableProcessors)) {
+
+            for (int i = 0; i < 30; i++) {
+                int index = i + 1;
+                threadPool.execute(() -> process(index));
+            }
+            threadPool.shutdown();
+        }
+    }
+
+    private static void process(int index) {
+        var random = new Random();
+        int minWaitTime = 5000; // 5秒（ミリ秒単位）
+        int maxWaitTime = 10000; // 10秒（ミリ秒単位）
+
+        // ランダムな待機時間を生成
+        int waitTime = random.nextInt(maxWaitTime - minWaitTime + 1) + minWaitTime;
+
+        try {
+            Thread.sleep(waitTime);
+            log.info("処理-%02d：%.3fsec".formatted(index, waitTime / 1000.0));
+
+        } catch (InterruptedException e) {
+            log.error("", e);
+        }
+    }
+}
